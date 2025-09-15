71bb995be7b927087999cc0b841c821dca3dc290
Iman Din <38789931+imandin408@users.noreply.github.com>
2025-09-15 19:22:51 -0400

    small fix

--- src/system/meta/CreditsPanel.h
+++ src/system/meta/CreditsPanel.h
@@ -11,42 +11,41 @@
 #include "ui/UIPanel.h"
 #include "utl/Symbol.h"
 
-class CreditsPanel : public UIListProvider, public UIPanel{
-    public:
-        OBJ_CLASSNAME(classname)
-        virtual void SetType(Symbol);
-        virtual void Text(int, int, UIListLabel *, UILabel *) const;
-        virtual int NumData() const;
-        virtual RndMat* Mat(int, int, UIListMesh *) const;
-        virtual DataNode Handle(DataArray *, bool);
+class CreditsPanel : public UIListProvider, public UIPanel {
+public:
+    OBJ_CLASSNAME(CreditsPanel)
+    OBJ_SET_TYPE(CreditsPanel)
+    virtual void Text(int, int, UIListLabel *, UILabel *) const;
+    virtual int NumData() const;
+    virtual RndMat *Mat(int, int, UIListMesh *) const;
+    virtual DataNode Handle(DataArray *, bool);
 
+protected:
+    DataNode OnMsg(ButtonDownMsg const &);
 
-    protected:
-        DataNode OnMsg(ButtonDownMsg const &);
-
-    private:
-        CreditsPanel();
-        ~CreditsPanel();
-        virtual bool IsLoaded() const;
-        virtual void Exit();
-        virtual void Enter();
-        void PausePanel(bool);
-        void DebugToggleAutoScroll();
-        virtual void Load();
-        virtual bool Exiting() const;
-        virtual void FinishLoad();
-        virtual void Unload();
-        virtual void Poll();
+private:
+    CreditsPanel();
+    ~CreditsPanel();
+    virtual bool IsLoaded() const;
+    virtual void Exit();
+    virtual void Enter();
+    void PausePanel(bool);
+    void DebugToggleAutoScroll();
+    virtual void Load();
+    virtual bool Exiting() const;
+    virtual void FinishLoad();
+    virtual void Unload();
+    virtual void Poll();
 
 #ifdef MILO_DEBUG
-    bool mCheatOn; 
+    bool mCheatOn;
 #endif
-    DataLoader *mLoader; 
-    DataArray *mNames; 
-    UIList *mList; 
-    Stream *mStream; 
-    bool mAutoScroll; //0x50
-    float mSavedSpeed; //0x54
+    DataLoader *mLoader;
+    DataArray *mNames;
+    UIList *mList;
+    Stream *mStream;
+    bool mAutoScroll; // 0x50
+    float mSavedSpeed; // 0x54
     /** Whether or not the panel is paused. */
-    bool mPaused; //0x58
+    bool mPaused; // 0x58
 };
