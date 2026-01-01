cb99689b19a61401f9dd560968463a85834e0f23
jsenior10 <jacobsenior4@gmail.com>
2026-01-01 11:54:46 +0000

    i finally read the readme lol

--- src/lazer/meta_ham/MQSongSortByCharacter.cpp
+++ src/lazer/meta_ham/MQSongSortByCharacter.cpp
@@ -7,28 +7,12 @@ int MQSongCharCmp::Compare(const NavListItemSortCmp *cmp, NavListNodeType type) 
 
     case kNodeHeader: {
         const MQSongCharCmp *mqCmp = cmp->GetMQSongCharCmp();
-        int iVar2 = unk8 - mqCmp->unk8;
-        const char *a = unk8;
-        const char *b = mqCmp->unk8;
-        for (int diff = iVar2; diff == 0; diff = (++a) - (++b)) {
-            if (a[diff] == '\0') {
-                return diff;
-            }
-        }
-        return a - b;
+        return strcmp(unk8, mqCmp->unk8);
     }
 
     case kNodeItem: {
         const MQSongCharCmp *mqCmp = cmp->GetMQSongCharCmp();
-        int iVar2 = unk4 - mqCmp->unk4;
-        const char *a = unk4;
-        const char *b = mqCmp->unk4;
-        for (int diff = iVar2; diff == 0; diff = (++a) - (++b)) {
-            if (a[diff] == '\0') {
-                return diff;
-            }
-        }
-        return a - b;
+        return strcmp(unk4, mqCmp->unk4);
     }
     default:
         MILO_FAIL("invalid type of node comparison.\n");
