00c630577c1a72d48dcb0160bac7dae537f2877a
jsenior10 <jacobsenior4@gmail.com>
2026-04-30 13:45:29 +0100

    debuggraph draw improvements

--- src/system/utl/DebugGraph.cpp
+++ src/system/utl/DebugGraph.cpp
@@ -24,70 +24,70 @@ void DebugGraph::Draw() {
     rnd->AddRectFilled2D(rect, mColorB);
 
     if (unk50) {
-        Vector2 minPos;
-        minPos.x = mRect.x;
-        minPos.y = mRect.y + mRect.h - 0.02f;
-        rnd->AddScreenString(MakeString<float>("%.3f", unk3c), minPos, mColorA);
+        Vector2 minPos(mRect.x, mRect.y + mRect.h - 0.02f);
+        rnd->AddScreenString(MakeString("%.3f", unk3c), minPos, mColorA);
 
-        Vector2 maxPos;
-        maxPos.x = mRect.x;
-        maxPos.y = mRect.y;
-        rnd->AddScreenString(MakeString<float>("%.3f", unk40), maxPos, mColorA);
+        Vector2 maxPos(mRect.x, mRect.y);
+        rnd->AddScreenString(MakeString("%.3f", unk40), maxPos, mColorA);
     }
 
     if (unk44 != FLT_MAX) {
-        Vector2 p1;
-        p1.x = mRect.x;
-        p1.y = mRect.y + mRect.h * (1.0f - clamp((unk44 - unk3c) / (unk40 - unk3c)));
+        Hmx::Color color(1.0f, 1.0f, 1.0f, 1.0f);
 
-        Vector2 p2;
-        p2.x = mRect.x + mRect.w;
-        p2.y = mRect.y + mRect.h * (1.0f - clamp((unk44 - unk3c) / (unk40 - unk3c)));
+        Vector2 p2(
+            mRect.w + mRect.x,
+            mRect.y + mRect.h * (1.0f - clamp((unk44 - unk3c) / (unk40 - unk3c)))
+        );
 
-        rnd->AddScreenLine(p1, p2, Hmx::Color(1.0f, 1.0f, 1.0f, 1.0f), false);
+        Vector2 p1(
+            mRect.x, mRect.y + mRect.h * (1.0f - clamp((unk44 - unk3c) / (unk40 - unk3c)))
+        );
 
-        Vector2 textPos;
-        textPos.x = mRect.x;
-        textPos.y = mRect.y + mRect.h * (1.0f - clamp((unk44 - unk3c) / (unk40 - unk3c)));
+        rnd->AddScreenLine(p1, p2, color, false);
+
+        Vector2 textPos(
+            mRect.x, mRect.y + mRect.h * (1.0f - clamp((unk44 - unk3c) / (unk40 - unk3c)))
+        );
 
         rnd->AddScreenString(
-            MakeString<float>("%.3f", unk44), textPos, Hmx::Color(1.0f, 1.0f, 1.0f, 1.0f)
+            MakeString("%.3f", unk44), textPos, Hmx::Color(1.0f, 1.0f, 1.0f, 1.0f)
         );
     }
 
     Vector2 titlePos;
-    titlePos.x = mRect.x + 0.1f;
+
     titlePos.y = mRect.y;
-    rnd->AddScreenString(unk48.c_str(), titlePos, Hmx::Color(1.0f, 1.0f, 1.0f, 1.0f));
+    Hmx::Color color(1.0f, 1.0f, 1.0f, 1.0f);
+    titlePos.x = mRect.x + 0.1f;
+
+    rnd->AddScreenString(unk48.c_str(), titlePos, color);
 
     if (!mSamples.empty()) {
         auto it = mSamples.begin();
         int idx = 1;
-        Vector2 prevPos;
-        prevPos.y =
-            mRect.y + mRect.h * (1.0f - clamp((it->data - unk3c) / (unk40 - unk3c)));
-        prevPos.x = mRect.x + mRect.w * (1.0f - clamp(0.0f / (float)(unk38 - 1)));
+        Vector2 prevPos(
+            mRect.x + mRect.w * (1.0f - clamp(0.0f / (float)(unk38 - 1))),
+            mRect.y + mRect.h * (1.0f - clamp((it->data - unk3c) / (unk40 - unk3c)))
+        );
 
         ++it;
 
         for (; it != mSamples.end(); ++it) {
-            Vector2 curPos;
-            curPos.y =
-                mRect.y + mRect.h * (1.0f - clamp((it->data - unk3c) / (unk40 - unk3c)));
-            curPos.x =
-                mRect.x + mRect.w * (1.0f - clamp((float)idx / (float)(unk38 - 1)));
+            Vector2 curPos(
+                mRect.x + mRect.w * (1.0f - clamp((float)idx / (float)(unk38 - 1))),
+                mRect.y + mRect.h * (1.0f - clamp((it->data - unk3c) / (unk40 - unk3c)))
+            );
 
             if (it->b) {
-                Vector2 hl_p1;
-                hl_p1.y = mRect.y + mRect.h;
+                Vector2 hl_p1(
+                    mRect.x + mRect.w * (1.0f - clamp((float)idx / (float)(unk38 - 1))),
+                    mRect.y + mRect.h
+                );
 
-                Vector2 hl_p2;
-                hl_p2.y = mRect.y;
-
-                hl_p1.x =
-                    mRect.x + mRect.w * (1.0f - clamp((float)idx / (float)(unk38 - 1)));
-                hl_p2.x =
-                    mRect.x + mRect.w * (1.0f - clamp((float)idx / (float)(unk38 - 1)));
+                Vector2 hl_p2(
+                    mRect.x + mRect.w * (1.0f - clamp((float)idx / (float)(unk38 - 1))),
+                    mRect.y
+                );
 
                 rnd->AddScreenLine(
                     hl_p2, hl_p1, Hmx::Color(1.0f, 1.0f, 1.0f, 1.0f), false
