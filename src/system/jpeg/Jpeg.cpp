0ff60063c7546002016c19d6f63dc47ce37683a6
rjkiv <76180273+rjkiv@users.noreply.github.com>
2026-05-22 14:43:57 -0700

    fully match jpeg

--- src/system/jpeg/Jpeg.cpp
+++ src/system/jpeg/Jpeg.cpp
@@ -9,10 +9,8 @@ namespace {
     void JpegInitDestination(jpeg_compress_struct *s) {
         jpeg_destination_mgr *dest = s->dest;
         MILO_ASSERT(dest, 0x8b);
-        // i have no idea if this is right
-        // it's assuming dest is an array of jpeg_destination_mgr structs
-        dest->next_output_byte = dest[1].next_output_byte;
-        dest->free_in_buffer = dest[1].free_in_buffer;
+        dest->next_output_byte = (JOCTET *)dest->buffer;
+        dest->free_in_buffer = dest->in_bytes;
     }
     unsigned char JpegEmptyOutputBuffer(jpeg_compress_struct *s) {
         MILO_ASSERT(false, 0x94);
@@ -21,7 +19,7 @@ namespace {
     void JpegTermDestination(jpeg_compress_struct *s) {
         jpeg_destination_mgr *dest = s->dest;
         MILO_ASSERT(dest, 0x9c);
-        dest[1].free_in_buffer = dest->free_in_buffer;
+        dest->out_bytes = dest->in_bytes - dest->free_in_buffer;
     }
 };
 
@@ -32,15 +30,15 @@ bool LoadBitmapIntoJpeg(char *pixels, int w, int h, int bpp, void *buffer, int &
     jpeg_CreateCompress(&compress, 0x3E, sizeof(jpeg_compress_struct));
     jpeg_destination_mgr dest_mgr;
     memset(&dest_mgr, 0, sizeof(jpeg_destination_mgr));
-    dest_mgr.next_output_byte = 0;
-    dest_mgr.free_in_buffer = 0;
+    dest_mgr.buffer = buffer;
     dest_mgr.init_destination = JpegInitDestination;
     dest_mgr.empty_output_buffer = JpegEmptyOutputBuffer;
     dest_mgr.term_destination = JpegTermDestination;
+    dest_mgr.in_bytes = bytes;
     compress.dest = &dest_mgr;
     compress.in_color_space = JCS_RGB;
-    compress.image_width = w;
     compress.image_height = h;
+    compress.image_width = w;
     compress.input_components = bpp;
     jpeg_set_defaults(&compress);
     jpeg_start_compress(&compress, true);
@@ -50,5 +48,6 @@ bool LoadBitmapIntoJpeg(char *pixels, int w, int h, int bpp, void *buffer, int &
         jpeg_write_scanlines(&compress, (JSAMPARRAY)&ptr, 1);
     }
     jpeg_finish_compress(&compress);
+    bytes = dest_mgr.out_bytes;
     return true;
 }
