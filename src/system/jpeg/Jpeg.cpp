6b6af338cd14d78c75d28105a46e8f4d1949935d
rjkiv <76180273+rjkiv@users.noreply.github.com>
2026-05-22 14:35:13 -0700

    jpeg.cpp attempt

--- src/system/jpeg/Jpeg.cpp
+++ src/system/jpeg/Jpeg.cpp
@@ -25,11 +25,30 @@ namespace {
     }
 };
 
-bool LoadBitmapIntoJpeg(char *, int, int, int, void *, int &) {
+bool LoadBitmapIntoJpeg(char *pixels, int w, int h, int bpp, void *buffer, int &bytes) {
     jpeg_error_mgr err;
-    jpeg_std_error(&err);
     jpeg_compress_struct compress;
+    compress.err = jpeg_std_error(&err);
     jpeg_CreateCompress(&compress, 0x3E, sizeof(jpeg_compress_struct));
-
+    jpeg_destination_mgr dest_mgr;
+    memset(&dest_mgr, 0, sizeof(jpeg_destination_mgr));
+    dest_mgr.next_output_byte = 0;
+    dest_mgr.free_in_buffer = 0;
+    dest_mgr.init_destination = JpegInitDestination;
+    dest_mgr.empty_output_buffer = JpegEmptyOutputBuffer;
+    dest_mgr.term_destination = JpegTermDestination;
+    compress.dest = &dest_mgr;
+    compress.in_color_space = JCS_RGB;
+    compress.image_width = w;
+    compress.image_height = h;
+    compress.input_components = bpp;
+    jpeg_set_defaults(&compress);
+    jpeg_start_compress(&compress, true);
+    int i1 = compress.input_components * w;
+    while (compress.next_scanline < compress.image_height) {
+        unsigned char *ptr = (unsigned char *)pixels + compress.next_scanline * i1;
+        jpeg_write_scanlines(&compress, (JSAMPARRAY)&ptr, 1);
+    }
+    jpeg_finish_compress(&compress);
     return true;
 }
