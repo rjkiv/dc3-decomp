ed2ad746666138d775ffa5d0e4b66ba8211759a2
rjkiv <76180273+rjkiv@users.noreply.github.com>
2026-05-30 12:51:54 -0700

    fftipp tweaks

--- src/system/synth_xbox/FftIpp.cpp
+++ src/system/synth_xbox/FftIpp.cpp
@@ -1,37 +1,31 @@
 #include "synth_xbox/FftIpp.h"
 #include "utl/MemMgr.h"
 
-void FftIpp::FftRealCcs(unsigned int *, volatile float &, unsigned int *, float &) {}
+// void FftIpp::FftRealCcs(float const *__restrict, float *__restrict) {}
 
-void FftIpp::FftReal(
-    unsigned int *param1, volatile float &param2, unsigned int *, float &, volatile float &
-) {}
-
-FftIpp::~FftIpp() {
-    if (unk8 != 0) {
-        MemFree(&unk8);
-    }
-
-    if (unk14 != 0) {
-        MemFree(&unk14);
-    }
-
-    if (unk20 != 0) {
-        MemFree(&unk20);
-    }
-
-    if (unk2c != 0) {
-        MemFree(&unk2c);
-    }
-
-    if (unk38 != 0) {
-        MemFree(&unk38);
-    }
-}
+// void FftIpp::FftReal(float const *__restrict, float *__restrict, float *__restrict) {}
 
 FftIpp::FftIpp()
     : unk0(0), unk4(0), unk8(0), unkc(0), unk10(0), unk14(0), unk18(0), unk1c(0),
       unk20(0), unk24(0), unk28(0), unk2c(0), unk30(0), unk34(0), unk38(0), unk3c(0),
       unk40(0) {}
 
-void FftIpp::SetMode(int) {}
+FftIpp::~FftIpp() {
+    if (unk38 != 0) {
+        MemFree(unk38);
+    }
+    if (unk2c != 0) {
+        MemFree(unk2c);
+    }
+    if (unk20 != 0) {
+        MemFree(unk20);
+    }
+    if (unk14 != 0) {
+        MemFree(unk14);
+    }
+    if (unk8 != 0) {
+        MemFree(unk8);
+    }
+}
+
+// void FftIpp::SetMode(int) {}
--- src/system/synth_xbox/FftIpp.h
+++ src/system/synth_xbox/FftIpp.h
@@ -2,28 +2,33 @@
 
 class FftIpp {
 public:
-    void FftRealCcs(unsigned int *, volatile float &, unsigned int *, float &);
-    void
-    FftReal(unsigned int *, volatile float &, unsigned int *, float &, volatile float &);
-    ~FftIpp();
     FftIpp();
+    ~FftIpp();
     void SetMode(int);
+    void FftReal(float const *__restrict, float *__restrict, float *__restrict);
+    void FftRealCcs(float const *__restrict, float *__restrict);
 
+private:
     int unk0;
     int unk4;
-    int unk8;
+    void *unk8; // std::vector<float,class XboxAllocator<float>>*
+
     int unkc;
     int unk10;
-    int unk14;
+    void *unk14; // std::vector<float,class XboxAllocator<float>>*
+
     int unk18;
     int unk1c;
-    int unk20;
+    void *unk20; // std::vector<float,class XboxAllocator<float>>*
+
     int unk24;
     int unk28;
-    int unk2c;
+    void *unk2c;
+
     int unk30;
     int unk34;
-    int unk38;
+    void *unk38; // std::vector<float,class XboxAllocator<float>>*
+
     int unk3c;
     int unk40;
 };
