10c6b07850423cebe867ec1a9f7ca457234493c0
rjkiv <76180273+rjkiv@users.noreply.github.com>
2026-05-31 10:14:43 -0700

    move appropriate TUs into synapse_apo

--- src/system/synth360/synapse_apo/IPP_basicmath_xbox.cpp
+++ src/system/synth360/synapse_apo/IPP_basicmath_xbox.cpp
@@ -1,4 +1,4 @@
-#include "synth360/IPP_basicmath_xbox.h"
+#include "synth360/synapse_apo/IPP_basicmath_xbox.h"
 
 namespace IPP {
     void Add_InPlace(unsigned int size, const float *src, float *dst) {
