b9e594a250dc3e45ea77f49d816f901cab2ffa19
rjkiv <76180273+rjkiv@users.noreply.github.com>
2026-06-23 20:44:58 -0700

    nearly match urlencode

--- src/system/utl/UrlEncode.cpp
+++ src/system/utl/UrlEncode.cpp
@@ -14,24 +14,21 @@ namespace {
 }
 
 void URLEncode(char const *input, String &output, bool escapeUnsafe) {
-    char const *reserved = "$&+,/:;=?@\"<>";
-    char const *unsafe = "#%{}|\\^~[]`";
-    char const *hexmap = "0123456789ABCDEF";
     int length = strlen(input);
-
-    for (int i = 0; i < length; ++i) {
+    for (int i = 0; i < length; i++) {
         char c = input[i];
-        if (IsCharInString(c, unsafe) || IsCharInString(c, reserved) || c < ' '
-            || c > '~') {
-            output += '%';
-
-            if (escapeUnsafe && (c < ' ' || c > '~')) {
-                output += "2";
-            } else {
-                output += hexmap[c >> 4];
-                c = hexmap[c & 0xf];
-            }
+        if (!IsCharInString(c, "#%{}|\\^~[]`") && !IsCharInString(c, "$&+,/:;=?@\"<>")
+            && c >= ' ' && c <= '~') {
             output += c;
+        } else {
+            output += "%";
+            if (escapeUnsafe && !(c >= ' ' && c <= '~')) {
+                output += "0123456789ABCDEF"[2];
+                output += "0123456789ABCDEF"[0];
+            } else {
+                output += "0123456789ABCDEF"[(c >> 4) & 0xF];
+                output += "0123456789ABCDEF"[c & 0xF];
+            }
         }
     }
 }
