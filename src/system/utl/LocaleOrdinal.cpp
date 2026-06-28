386441b04f75717ff02f8d5ac13cb4c51b5256b4
rjkiv <76180273+rjkiv@users.noreply.github.com>
2026-06-27 17:31:02 -0700

    match localeordinal

--- src/system/utl/LocaleOrdinal.cpp
+++ src/system/utl/LocaleOrdinal.cpp
@@ -1,21 +1,24 @@
 #include "LocaleOrdinal.h"
-
 #include "UTF8.h"
 #include "os/System.h"
 
-const char *LocalizeOrdinal(int num, LocaleGender gender, LocaleNumber number, bool superscriptMarkup, Symbol lang, Locale &locale) {
+const char *LocalizeOrdinal(
+    int num,
+    LocaleGender gender,
+    LocaleNumber number,
+    bool superscriptMarkup,
+    Symbol lang,
+    Locale &locale
+) {
     char buf[255];
-    buf[0] = *gNullStr;
-    char code1, code2;
-    if (!lang) {
+    if (lang.Null()) {
         lang = SystemLanguage();
     }
-    strncpy(buf, LocalizeSeparatedInt(num, locale), 255);
+    strncpy(buf, LocalizeSeparatedInt(num, locale), sizeof(buf));
+    buf[sizeof(buf) - 1] = 0;
     int len = strlen(buf);
-    if (len > 0) code1 = buf[len - 1];
-    else code1 = '0';
-    if (len > 1) code2 = buf[len - 2];
-    else code2 = '0';
+    char code1 = len > 0 ? buf[len - 1] : '0';
+    char code2 = len > 1 ? buf[len - 2] : '0';
 
     static Symbol jpn("jpn");
     static Symbol eng("eng");
@@ -26,35 +29,58 @@ const char *LocalizeOrdinal(int num, LocaleGender gender, LocaleNumber number, b
 
     if (lang != jpn) {
         if (lang == eng) {
-            if (superscriptMarkup) strcat(buf, "<sup>");
-            if (code1 == '1' && code2 != '1') strcat(buf, "st");
-            else if (code2 == '2' && code2 != '1') strcat(buf, "nd");
-            else if (code1 == '3' && code2 != '1') strcat(buf, "rd");
-            else strcat(buf, "th");
-            if (superscriptMarkup) strcat(buf, "</sup>");
-        }
-        else if (lang == fre) {
-            if (superscriptMarkup) strcat(buf, "<sup>");
-            if (code1 == code2) {
-                if (gender == LocaleGenderMasculine) strcat(buf, "er");
-                // looks like there may be something else happening here but i cant see it
-                else strcat(buf, "re");
+            if (superscriptMarkup) {
+                strcat(buf, "<sup>");
             }
-            else strcat(buf, "e");
-            if (superscriptMarkup) strcat(buf, "</sup>");
-        }
-        else if (lang == deu) strcat(buf, ".");
-        else if (lang == esl || lang == ita) {
+            if (code1 == '1' && code2 != '1') {
+                strcat(buf, "st");
+            } else if (code1 == '2' && code2 != '1') {
+                strcat(buf, "nd");
+            } else if (code1 == '3' && code2 != '1') {
+                strcat(buf, "rd");
+            } else {
+                strcat(buf, "th");
+            }
+            if (superscriptMarkup) {
+                strcat(buf, "</sup>");
+            }
+        } else if (lang == fre) {
+            if (superscriptMarkup) {
+                strcat(buf, "<sup>");
+            }
+            if (streq(buf, "1")) {
+                if (gender == LocaleGenderMasculine) {
+                    strcat(buf, "er");
+                } else {
+                    strcat(buf, "re");
+                }
+            } else {
+                strcat(buf, "e");
+            }
+            if (superscriptMarkup) {
+                strcat(buf, "</sup>");
+            }
+        } else if (lang == deu) {
+            strcat(buf, ".");
+        } else if (lang == esl || lang == ita) {
             String str;
             EncodeUTF8(str, 0xb0);
             if (gender == LocaleGenderMasculine) {
-                if (number == 0) strcat(buf, str.c_str());
-                else { strcat(buf, str.c_str()); strcat(buf, "s"); }
+                if (number == 0) {
+                    strcat(buf, str.c_str());
+                } else {
+                    strcat(buf, str.c_str());
+                    strcat(buf, "s");
+                }
+            } else if (number == 0) {
+                strcat(buf, str.c_str());
+            } else {
+                strcat(buf, str.c_str());
+                strcat(buf, "s");
             }
-            else if (number == 0) strcat(buf, str.c_str());
-            else { strcat(buf, str.c_str()); strcat(buf, "s"); }
+        } else {
+            MILO_NOTIFY("Localizing Ordinal for unsupported language %s", lang);
         }
-        else MILO_NOTIFY("Localizing Ordinal for unsupported language %s", lang);
     }
     return MakeString(buf);
 }
