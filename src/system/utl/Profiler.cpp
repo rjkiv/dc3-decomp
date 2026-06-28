2d3accb9b80abcc38e737781d11b29ebc999c8e0
rjkiv <76180273+rjkiv@users.noreply.github.com>
2026-06-27 17:40:57 -0700

    match profiler

--- src/system/utl/Profiler.cpp
+++ src/system/utl/Profiler.cpp
@@ -2,21 +2,21 @@
 #include "math/Utl.h"
 #include "os/Debug.h"
 #include "os/Timer.h"
+#include <float.h>
 
 Profiler::Profiler(char const *c, int i)
-    : mName(c), mMin(3.4028235e+38), mMax(0.0f), mSum(0.0f), mCount(0), mCountMax(i) {}
+    : mName(c), mMin(FLT_MAX), mMax(0.0f), mSum(0.0f), mCount(0), mCountMax(i) {}
 
 void Profiler::Start() { mTimer.Start(); }
 
 void Profiler::Stop() {
     mTimer.Stop();
-    mMin = Min(mMin, mTimer.Ms());
-    mMax = Max(mMax, mTimer.Ms());
-    mCount++;
+    MinEq(mMin, mTimer.Ms());
+    MaxEq(mMax, mTimer.Ms());
     mSum += mTimer.Ms();
-    if (mCount == mCountMax) {
-        if (mCountMax == 1U) {
-            MILO_LOG("%s: %s\n", FormatTime(mMin));
+    if (++mCount == mCountMax) {
+        if (mCountMax == 1) {
+            MILO_LOG("%s: %s\n", mName, FormatTime(mMin));
         } else {
             MILO_LOG(
                 "%s: min %s max %s mean %s\n",
@@ -27,7 +27,7 @@ void Profiler::Stop() {
             );
         }
         mCount = 0;
-        mMin = 3.402823e+38;
+        mMin = FLT_MAX;
         mMax = 0;
         mSum = 0;
     }
--- src/system/utl/Profiler.h
+++ src/system/utl/Profiler.h
@@ -8,11 +8,11 @@ public:
     void Stop();
 
 private:
-    char const *mName; // 0x0
+    const char *mName; // 0x0
     Timer mTimer; // 0x8
     float mMin; // 0x38
     float mMax; // 0x3c
     float mSum; // 0x40
-    int mCount; // 0x44
-    int mCountMax; // 0x48
+    unsigned int mCount; // 0x44
+    unsigned int mCountMax; // 0x48
 };
