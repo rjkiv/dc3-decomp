d8bb16a080849811433200e42a755fc71a2b9014
rjkiv <76180273+rjkiv@users.noreply.github.com>
2026-09-22 21:18:38 -0700

    match hamscrollspeedindicator

--- src/system/hamobj/HamScrollSpeedIndicator.cpp
+++ src/system/hamobj/HamScrollSpeedIndicator.cpp
@@ -45,8 +45,7 @@ BEGIN_COPYS(HamScrollSpeedIndicator)
 END_COPYS
 
 void HamScrollSpeedIndicator::DrawShowing() {
-    for (std::vector<RndDrawable *>::iterator it = mDraws.begin(); it != mDraws.end();
-         ++it) {
+    FOREACH (it, mDraws) {
         (*it)->Draw();
     }
 }
@@ -78,8 +77,7 @@ void HamScrollSpeedIndicator::Show(bool enter) {
 
 void HamScrollSpeedIndicator::Draw(const Transform &xfm) {
     SetWorldXfm(xfm);
-    for (std::vector<RndDrawable *>::iterator it = mDraws.begin(); it != mDraws.end();
-         ++it) {
+    FOREACH (it, mDraws) {
         (*it)->Draw();
     }
 }
@@ -90,19 +88,47 @@ void HamScrollSpeedIndicator::PreLoad(BinStream &bs) {
     LOAD_REVS(bs);
     ASSERT_REVS(2, 0)
     RndDir::PreLoad(bs);
-    bs.PushRev(packRevs(d.altRev, d.rev), this);
+    d.PushRev(this);
 }
 
 void HamScrollSpeedIndicator::PostLoad(BinStream &bs) {
     BinStreamRev d(bs, bs.PopRev(this));
     RndDir::PostLoad(bs);
-    if ((d.rev & 0xffff) >= 1) {
-        bs >> mEnterAnim;
-        bs >> mExitAnim;
-        bs >> mIndicatorAnim;
+    if (d.rev >= 1) {
+        d >> mEnterAnim;
+        d >> mExitAnim;
+        d >> mIndicatorAnim;
     }
-    if (2 <= (d.rev & 0xffff)) {
-        bs >> mSlowScrollThresholdFrame;
-        bs >> mFastScrollThresholdFrame;
+    if (d.rev >= 2) {
+        d >> mSlowScrollThresholdFrame;
+        d >> mFastScrollThresholdFrame;
+    }
+}
+
+void HamScrollSpeedIndicator::Update(float f1, float f2, float f3) {
+    float range = (mIndicatorAnim->EndFrame() - mIndicatorAnim->StartFrame()) / 2;
+    float f9;
+
+    if (f1 >= 0.1f && f1 <= 0.9f) {
+        f9 = (f1 - 0.1f) * 1.25f;
+        f9 = -(f9 * mSlowScrollThresholdFrame * 2 - mSlowScrollThresholdFrame);
+    } else {
+        if (f1 > 0.5f) {
+            float fvar3 = range - mSlowScrollThresholdFrame;
+            f1 -= 0.9f;
+            f3 = f3 + 1 - 0.9f;
+            f9 = -(f1 / f3);
+            f9 = f9 * fvar3 - mSlowScrollThresholdFrame;
+        } else {
+            float fvar3 = range - mSlowScrollThresholdFrame;
+            f1 -= 0.1f;
+            f2 += 0.1f;
+            f9 = -(f1 / f2);
+            f9 = f9 * fvar3 + mSlowScrollThresholdFrame;
+        }
+    }
+    if (unk1fc) {
+        float frame = Clamp(mIndicatorAnim->StartFrame(), mIndicatorAnim->EndFrame(), f9);
+        mIndicatorAnim->SetFrame(frame, 1);
     }
 }
